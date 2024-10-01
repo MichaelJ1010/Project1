b67de680fd70307fbdf0630e29cfdf65daa588d4
Michael J <30031129@cc.peralta.edu>
2024-09-30 20:45:32 -0700

    can now see what items you buy

--- Shop.cpp
+++ Shop.cpp
@@ -7,18 +7,17 @@ using namespace std;
 
     string itemNames[] = {"apple", "orange", "strawberry"};
     double itemValues[] = {2.50, 2.00, 3.50};
+    string dailyItems[3];
+    double dailyPrices[3];
 
 
 
 
 void randomizeItems(string items[]){
-    
-    
-    
+        
     for(int i = 0; i < 3; i++){
     
     int n = rand() % 3;
-    
 
     items[i] = itemNames[n];
     for(int j = 0; j < i; j++){
@@ -42,63 +41,69 @@ void randomizePrice(double prices[]){
         i--;
         break;
     }
-    }   
-    
+    }    
  }
-    
-    
-
 }
 
 void printItemsAndPrices(string items[], double prices[]){
     for(int i = 0; i < 3; i++){
-        cout << setw(50) << items[i] << " - " << setprecision(2) << fixed << "$" << prices[i] << endl;
+        cout << i + 1 << ") " << items[i] << " - " << setprecision(2) << fixed << "$" << prices[i] << setw(10);
     }
 }
 
-
+void buyItems(int itemNumber){
+    switch(itemNumber){
+        case 1:
+        cout << "Bought " << dailyItems[0] << " for " << setprecision(2) << fixed << "$" << dailyPrices[0] << endl;
+        break;
+        case 2:
+        cout << "Bought " << dailyItems[1] << " for " << setprecision(2) << fixed << "$" << dailyPrices[1] << endl;
+        break;
+        case 3:
+        cout << "Bought " << dailyItems[2] << " for " << setprecision(2) << fixed << "$" << dailyPrices[2] << endl;
+        break;
+    }
+}
 void buyItems(){
     bool continueShopping = true;
-    cout << "Press the number of the item you would like to buy. If you are done, press 0" << endl;
+    cout << "Press the number of the item you would like to buy. If you are done, press 4" << endl;
     do{
         int playerChoice;
-        
         cin >> playerChoice;
+        
+
         switch(playerChoice){
             case 1:
+            buyItems(playerChoice);
             break;
             case 2:
+            buyItems(playerChoice);
             break;
             case 3:
+            buyItems(playerChoice);
             break;
-            case 0:
+            case 4:
             continueShopping = false;
             break;
             default:
             cout << "Error, not a valid option." << endl;
             break;
         }
+        if(continueShopping){
+            printItemsAndPrices(dailyItems, dailyPrices);
+            cout << setw(0) << endl;  
+        }
+        
     
     }while(continueShopping);
 }
-void buyItems(int itemNumber){
-    switch(itemNumber){
-        case 1:
-        break;
-        case 2:
-        break;
-        case 3:
-        break;
-    }
-}
+
 
 
 
 int main(){
     double balance = 50.00;
         srand(time(0));
-        string dailyItems[3];
-        double dailyPrices[3];
         cout << "Welcome to the Shop!" << endl << "Here's what's available today: " << endl;
         randomizeItems(dailyItems);
         randomizePrice(dailyPrices);
