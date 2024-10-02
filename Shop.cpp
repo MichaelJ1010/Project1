2841c74561193ba040ca8fd595f75b25b9742782
Michael J <30031129@cc.peralta.edu>
2024-10-01 23:42:21 -0700

    improved edge cases while buying items

--- Shop.cpp
+++ Shop.cpp
@@ -5,17 +5,17 @@
 
 using namespace std;
 
-    const string itemNames[] = {"apple", "orange", "strawberry", "bannanna", "grape"};
-    const double itemValues[] = {2.50, 2.00, 3.50, 1.50, 3.00};
-    double currentBalance = 50.00;
+const string itemNames[] = {"apple", "orange", "strawberry", "bannanna", "grape"};
+const double itemValues[] = {2.50, 2.00, 3.50, 1.50, 3.00};
+double currentBalance = 50.00;
     
 
 void randomizeItems(string items[]);
 void randomizePrice(double prices[]);
 void randomizeQuantity(int quantity[]);
-void printItemsAndPrices(string items[], double prices[]);
-void buyItems(int itemNumber, double dailyPrices[], string dailyItems[]);
-void buyItems(double dailyPrices[], string dailyItems[]);
+void printQuantityItemPrices(int quantity[], string items[], double prices[]);
+void buyItems(int itemNumber, int dailyQuantity[], double dailyPrices[], string dailyItems[]);
+void buyItems(int dailyQuantity[], double dailyPrices[], string dailyItems[]);
 void startGame();
 string getDay(int day);
 
@@ -25,11 +25,11 @@ int main(){
     return 0;
 }
 
-void startGame(){
-    int trackDay = 1;
+void startGame(){ //sets correct day and randomizes shop items
+    short int trackDay = 1;
     string dailyItems[3];
     double dailyPrices[3];
-    int itemQuantity[3];
+    int dailyQuantity[3];
     
     while(true){
     
@@ -38,17 +38,21 @@ void startGame(){
     if(trackDay == 8){
         trackDay = 1;
     }
+
     randomizeItems(dailyItems);
     randomizePrice(dailyPrices);
-    randomizeQuantity(itemQuantity);
-    printItemsAndPrices(dailyItems, dailyPrices);
-    buyItems(dailyPrices, dailyItems);
+    randomizeQuantity(dailyQuantity);
+   printQuantityItemPrices(dailyQuantity,dailyItems,dailyPrices);
+    buyItems(dailyQuantity, dailyPrices, dailyItems);
+    
     if(trackDay == 10){
         break;
     }
     }
 }
-string getDay(int day){
+
+
+string getDay(int day){ //returns the day we are on
     if(day % 7 == 0){
         return "Sunday";
     }
@@ -78,13 +82,13 @@ string getDay(int day){
 }
 
 
-void randomizeItems(string items[]){
+void randomizeItems(string items[]){ //randomizes items for the current day
         
     for(int i = 0; i < 3; i++){
     
-    int n = rand() % 3;
+    short int randomNumber  = rand() % 3;
 
-    items[i] = itemNames[n];
+    items[i] = itemNames[randomNumber];
     for(int j = 0; j < i; j++){
     if(items[i] == items[j]){
         i--;
@@ -94,13 +98,14 @@ void randomizeItems(string items[]){
  }
 }
 
-void randomizePrice(double prices[]){
+
+void randomizePrice(double prices[]){ //randomizes prices for the current day
     
     for(int i = 0; i < 3; i++){
     
-    int n = rand() % 3;
+    short int randomNumber = rand() % 3;
     
-    prices[i] = itemValues[n];
+    prices[i] = itemValues[randomNumber];
     for(int j = 0; j < i; j++){
     if(prices[i] == prices[j]){
         i--;
@@ -110,49 +115,54 @@ void randomizePrice(double prices[]){
  }
 }
 
-void randomizeQuantity(int quantity[]){
+
+void randomizeQuantity(int quantity[]){ //randomizes quantities for the current day
     for(int i = 0; i < 3; i++){
     quantity[i] = (rand() % 3) + 1;
     }    
  }
 
-void printItemsAndPrices(string items[], double prices[]){
+
+void printQuantityItemPrices(int quantity[], string items[], double prices[]){ //formats items and prices nicely for the user
     for(int i = 0; i < 3; i++){
         cout << i + 1 << ") " << items[i] << " - " << setprecision(2) << fixed << "$" << prices[i] << setw(10);
     }
 }
 
-void buyItems(int itemNumber, double dailyPrices[], string dailyItems[]){
+
+void buyItems(int itemNumber,int dailyQuantity[], double dailyPrices[], string dailyItems[]){ //determines if user can buy item
     double futureBalance = currentBalance - dailyPrices[itemNumber - 1];
-    if(futureBalance < 0){
+    if(itemNumber == 0){
+            return;
+        }else if(futureBalance < 0 ){
             cout << "Oops! You don't have enough to buy that!" << endl;
         }else{
             currentBalance = futureBalance;
-            cout << "Bought " << dailyItems[itemNumber - 1] << " for " << setprecision(2) << fixed << "$" << dailyPrices[itemNumber - 1] << endl;
-            
+            cout << "Bought " << dailyItems[itemNumber - 1] << " for " << setprecision(2) << fixed << "$" << dailyPrices[itemNumber - 1] << endl;            
         }
 }
-void buyItems(double dailyPrices[], string dailyItems[]){
+
+
+void buyItems(int dailyQuantity[], double dailyPrices[], string dailyItems[]){ //determines what item user is trying to buy
     bool continueShopping = true;
     cout << endl << "You are starting the day off with $" << currentBalance << endl;
     cout << "Press the number of the item you would like to buy. If you are done, press 4" << endl;
     do{
         cout << "*****************************************************************************" << endl;
-        int playerChoice;
+        char playerChoice;
         cin >> playerChoice;
         
-
         switch(playerChoice){
-            case 1:
-            buyItems(playerChoice, dailyPrices, dailyItems);
+            case '1':
+            buyItems(1, dailyQuantity, dailyPrices, dailyItems);
             break;
-            case 2:
-            buyItems(playerChoice, dailyPrices, dailyItems);
+            case '2':
+            buyItems(2, dailyQuantity, dailyPrices, dailyItems);
             break;
-            case 3:
-            buyItems(playerChoice, dailyPrices, dailyItems);
+            case '3':
+            buyItems(3, dailyQuantity, dailyPrices, dailyItems);
             break;
-            case 4:
+            case '4':
             continueShopping = false;
             break;
             default:
@@ -161,7 +171,7 @@ void buyItems(double dailyPrices[], string dailyItems[]){
         }
         
         if(continueShopping){
-            printItemsAndPrices(dailyItems, dailyPrices);
+            printQuantityItemPrices(dailyQuantity,dailyItems,dailyPrices);
             cout << setw(0) << endl;  
             cout << "Current balance: " << currentBalance << endl;
         }
